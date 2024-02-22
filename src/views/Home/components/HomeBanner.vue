3f37a17e0155d384eadfb50b131bf0020aebf986
wjt <1@.com>
2024-02-23 00:45:11 +0800

    banner

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,7 +1,53 @@
+<script setup>
+// import {getBannerAPI} from '@/apis/home'
+// import {ref,onMounted} from 'vue'
+
+// const bannerList=ref([])
+
+// const getBanner = async ()=>{
+//     const res = await getBannerAPI()
+// bannerList.value = res .result
+
+// }
+// onMounted {()=>getBanner()
+// }
+import {ref} from 'vue'
+
+const bannerList=ref([
+    {id:'01',picture:'src/assets/images/banner1.jpg'},
+    {id:'02',picture:'src/assets/images/banner2.jpg'},
+    {id:'03',picture:'src/assets/images/banner3.jpg'},
+    {id:'04',picture:'src/assets/images/banner4.png'},
+    {id:'05',picture:'src/assets/images/banner5.png'}
+]) 
+</script>
+
+
+
 <template>
-    <div>
-       banner
-       
-        
-    </div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.picture" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 990px;
+  height: 480px;
+  position: absolute;
+  left:250px;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 480px;
+  }
+}
+</style>
